0fce9f88f8a15f0deda080792cfb3fade175144e
MathDevWeb <140265706+MathDevWeb@users.noreply.github.com>
2024-01-20 00:28:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
   const [showQuickView, setShowQuickView] = useState(false);
   const handleQuickViewClick = () => {
     setShowQuickView(!showQuickView);
-    setSelectedDay(false)
   };
 
   const handleSubmit = (e) => {
